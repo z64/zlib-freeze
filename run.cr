4ce0f8f1aafdc85d8b6cf3d3ff28878cc666c4a9
Zac Nowicki <zachnowicki@gmail.com>
2018-08-20 16:54:40 +0000

    Add spawn example

--- run.cr
+++ run.cr
@@ -1,5 +1,17 @@
 require "zlib"
 
-File.open("data.bin", "r") do |io|
-  Zlib::Reader.new(io).gets_to_end
+spawn do
+  i = 0
+  loop do
+    puts "[#{i}] looping"
+    i += 1
+    sleep 1
+  end
+end
+
+# Wait 3 sec to start
+sleep 3
+
+File.open("data.bin", "r") do |file|
+  Zlib::Reader.new(file).gets_to_end
 end
