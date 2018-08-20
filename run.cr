05cf0fb12350cefed7e1c4bb7c7f4c624d3f9626
Zac Nowicki <zachnowicki@gmail.com>
2018-08-20 12:07:46 +0000

    Always return nil from IO#peek

--- run.cr
+++ run.cr
@@ -1,5 +1,14 @@
 require "zlib"
 
-File.open("data.bin", "r") do |io|
+class NoPeekIO < IO::Memory
+  def peek
+    nil
+  end
+end
+
+File.open("data.bin", "r") do |file|
+  io = NoPeekIO.new
+  IO.copy(file, io)
+  io.rewind
   Zlib::Reader.new(io).gets_to_end
 end
